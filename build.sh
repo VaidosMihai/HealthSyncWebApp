47bcc0b1be6b8517c9a6f9f6b793242c527bcb58
Mihai Vaidoș <mihaita_star@yahoo.com>
2024-05-17 22:45:32 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Ensure we're using the correct Node.js version
-source ~/.nvm/nvm.sh
+export NVM_DIR="$HOME/.nvm"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+source "$NVM_DIR/nvm.sh"
 nvm install 20.13.1
 nvm use 20.13.1
 
