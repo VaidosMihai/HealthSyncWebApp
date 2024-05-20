dcbb2cfb0f1cb3cc38581ebd19960737e66e589c
Vaidos Mihai-Iulian <mihaita_star@yahoo.com>
2024-05-20 22:12:26 +0300

    Search page update

--- Frontend/FrontMedicalApplication/src/app/core/models/search-result/search-result.component.ts
+++ Frontend/FrontMedicalApplication/src/app/core/models/search-result/search-result.component.ts
@@ -35,36 +35,6 @@ export class SearchResultComponent implements OnInit {
     }); 
   }
 
-  //FIRST VERSION
-  // performSearch(): void {
-  //   this.searchService.search(this.searchTerm, this.entityType).subscribe({
-  //     next: (results) => {
-  //       this.searchResults = results;
-  //     },
-  //     error: (error) => {
-  //       // Log the error to the console
-  //       console.error('Error performing search:', error);
-  //       // Optionally, show the error to the user
-  //       alert('Error performing search: ' + error.message);
-  //     }
-  //   });
-  // }
-  
-  // SECOND VERSION
-  // performSearch(): void {
-  //   this.searchService.search(this.searchTerm, this.entityType).subscribe({
-  //     next: (results) => {
-  //       // Assuming the result is an array directly
-  //       //this.searchResults = { users: results }; // Adjust depending on actual response structure
-  //       this.searchResults = results;
-  //     },
-  //     error: (error) => {
-  //       console.error('Error performing search:', error);
-  //       alert('Error performing search: ' + error.message);
-  //     }
-  //   });
-  // }
-
   performSearch(): void {
     if (!this.entityType || !this.searchTerm) {
       console.error('Search term or entity type is missing.');
